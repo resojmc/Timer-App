7cd20951d555058972d6654b9fca0ba144eea4be
resojmc <resojmc@gmail.com>
2023-04-24 13:54:20 -0700

    Update Timer app.py

--- Timer app.py
+++ Timer app.py
@@ -5,6 +5,7 @@ import time, datetime
 root = tk.Tk()
 root.geometry("250x250")
 root.title("Timer")
+root.resizable(False, False)
 
 
 def countdown(hour, mins, secs):
@@ -12,41 +13,48 @@ def countdown(hour, mins, secs):
     timer = datetime.timedelta(seconds = total_seconds)
     time_label.config(text=f"{timer}", font=("Ariel", 30))
     if total_seconds > 0:
-        root.after(1000, countdown, hour, mins, secs-1)
+        time_label.time = root.after(1000, countdown, hour, mins, secs-1)
     else:
-        time_label.config(text="Time's up!", font=("Ariel", 30))
-        
+        time_label.config(text=f"00:00:00")
 
-def start_timer():
+def start_stop_timer():
+    if hasattr(time_label, 'time'):
+        root.after_cancel(time_label.time)
     hour = int(hour_entry.get())
     mins = int(min_entry.get())
     secs = int(sec_entry.get())
-    time_label.config(text=f"{hour}:{mins}:{secs}", font=("Ariel", 30))
     countdown(hour, mins, secs)
 
-
-time_label = tk.Label(root, text=f"0:00:00", font=("Ariel", 30))
+def reset_button():
+    if hasattr(time_label, 'time'):
+        root.after_cancel(time_label.time)
+    time_label.config(text=f"00:00:00")
+    
+time_label = tk.Label(root, text=f"00:00:00", font=("Ariel", 30))
 time_label.pack()
 
-start_button = tk.Button(root, text="Start Timer", command=start_timer)
+reset_button = tk.Button(root, text="Reset", command=reset_button)
+reset_button.place(x=205, y=190)
+
+start_button = tk.Button(root, text="Start Timer", command=start_stop_timer)
 start_button.place(x=177, y=220)
 
 hour_label = tk.Label(root, text="Hours: ", font=("Ariel", 10))
 hour_label.place(x=0, y=176)
 
-hour_entry = tk.Entry(root, width=10)
+hour_entry = tk.Spinbox(root, width=int(2.5), from_=0, to=12, state="readonly")
 hour_entry.place(x=45, y=178.5)
 
 min_label = tk.Label(root, text="Minutes: ", font=("Ariel", 10))
 min_label.place(x=0, y=200)
 
-min_entry = tk.Entry(root, width=10)
+min_entry = tk.Spinbox(root, width=int(2.5), from_=0, to=60, state="readonly")
 min_entry.place(x=56, y=202.5)
 
 sec_label = tk.Label(root, text="Seconds: ", font=("Ariel", 10))
 sec_label.place(x=0, y=225)
 
-sec_entry = tk.Entry(root, width=10)
+sec_entry = tk.Spinbox(root, width=int(2.5), from_=0, to=60, state="readonly")
 sec_entry.place(x=61.5, y=226.5)
 
 root.mainloop()
